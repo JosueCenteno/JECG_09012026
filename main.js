5f55ff633d8ea3a9d35f633c7da69b0f46b0a475
Josue <josuecenteno16@hotmail.comgit config --global user.email josuecenteno16@hotmail.com>
2026-01-11 16:16:12 -0600

    Formulario registro con clase Persona

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ btnMultiplicar.addEventListener("click", () => {
 
     document.getElementById("resultado").value = resultado;
 });
+
+
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    getNombreCompleto() {
+        return `${this.nombre} ${this.apellido}`;
+    }
+}
+
+const btnRegistrar = document.getElementById("btnRegistrar");
+
+btnRegistrar.addEventListener("click", () => {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+
+    const persona = new Persona(nombre, apellido);
+    document.getElementById("nombreCompleto").value = persona.getNombreCompleto();
+});
